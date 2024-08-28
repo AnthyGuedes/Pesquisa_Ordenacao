e18885a57da3c07a70ad92b099c71f3a8df9a9cb
Anthony <142349999+AnthyGuedes@users.noreply.github.com>
2024-08-28 08:31:11 -0300

    Add files via upload

---
+++ PesqOrd/Insercao.py
@@ -0,0 +1,12 @@
+def insercao(lista=[]):
+    for i in range(1,len,(lista)):
+        aux = lista[i]
+        for j in range(i-1,0,-1):
+            qtd_comparacoes += 1
+            if(aux < lista[j]):
+                lista[j+1] = lista[j]
+                qtd_trocas += 1
+            else:
+                break     
+        lista[j+1] = aux
+        qtd_trocas += 1
